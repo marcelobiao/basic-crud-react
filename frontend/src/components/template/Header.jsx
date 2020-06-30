64f02b52505828585d0a6217b3dff47b7ca7f082
Marcelo Biao <marcelobiao2@gmail.com>
2020-06-30 15:26:15 -0300

    Criando component Header

--- frontend/src/components/template/Header.jsx
+++ frontend/src/components/template/Header.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import './Header.css'
 
 export default props =>
-    <aside className="header">
-        Cabeçalho
+    <aside className="header d-nome d-sm-flex flex-column">
+        <h1 className="mt-3">
+            <i className={`fa fa-${props.icon}`}></i> {props.title}
+        </h1>
+        <p className="lead text-muted">{props.subtitle}</p>
     </aside>
