0ef2768c6c282d374368e7f7e3291f6daeca3d25
Marcelo Biao <marcelobiao2@gmail.com>
2020-06-30 15:31:02 -0300

    Corrigindo bug no component Header

--- frontend/src/components/template/Header.jsx
+++ frontend/src/components/template/Header.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import './Header.css'
 
 export default props =>
-    <aside className="header d-nome d-sm-flex flex-column">
+    <header className="header d-nome d-sm-flex flex-column">
         <h1 className="mt-3">
             <i className={`fa fa-${props.icon}`}></i> {props.title}
         </h1>
         <p className="lead text-muted">{props.subtitle}</p>
-    </aside>
+    </header>
