58ab596d2b8a4a7c28e2270d966d8131412fd428
Marcelo Biao <marcelobiao2@gmail.com>
2020-06-30 15:30:43 -0300

    Ajustando component Main

--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
 import './App.css'
 import Logo from '../components/template/Logo'
 import Nav from '../components/template/Nav'
@@ -9,6 +11,7 @@ export default props =>
     <div className="app">
         <Logo/>
         <Nav/>
-        <Main/>
+        <Main icon="home" title="início"
+            subtitle="Segundo projeto do capítulo de React."/>
         <Footer/>
     </div>
